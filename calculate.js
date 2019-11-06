85154c39b422714d59097b3c6c779fd9d20ce688
Lachlan-Desktop <lachlanmaclean97@gmail.com>
2019-11-07 02:19:18 +1300

    Iteration 3+4+5 bootstrap front end upload functionality implemented using multer RESTful API functional

--- calculate.js
+++ calculate.js
@@ -1,71 +1,78 @@
 //Lachlan Maclean BCPR280 Assignment 2
-var fs = require('fs');
 //Node Application that calculates correlation
 
 //let x and y be the file inserted
-var x = fs.readFileSync('testData/testData1.txt').toString('utf-8').split('\r\n');
-var y = fs.readFileSync('testData/testData2.txt').toString('utf-8').split('\r\n');
+//const x = fs.readFileSync('testData/testData1.txt').toString('utf-8').split('\r\n');
+//const y = fs.readFileSync('testData/testData2.txt').toString('utf-8').split('\r\n');
 
 //calculate n
-let n = x.length;
+//calculate(x,y);
+function calculate(x,y) {
+
+
+    let n = x.length;
 
 //calculate x^2 and y^2
-let x2 = x.map(v => Math.pow(v,2));
-let y2 = y.map(v => Math.pow(v,2));
+    let x2 = x.map(v => Math.pow(v, 2));
+    let y2 = y.map(v => Math.pow(v, 2));
 
 //Calculate signma x
-let sumx = 0;
-for (i in x){
-    sumx = sumx + parseFloat(x[i]);
-}
+    let sumx = 0;
+    for (i in x) {
+        sumx = sumx + parseFloat(x[i]);
+    }
 //Calculate signma y
-let sumy = 0;
-for (i in y){
-    sumy = sumy + parseFloat(y[i]);
-}
+    let sumy = 0;
+    for (i in y) {
+        sumy = sumy + parseFloat(y[i]);
+    }
 
 //calculate average of x
-let avgx = 0;
-avgx = sumx / x.length;
+    let avgx = 0;
+    avgx = sumx / x.length;
 //Calculate average of y
-let avgy = 0;
-avgy = sumy / y.length;
+    let avgy = 0;
+    avgy = sumy / y.length;
 
 //calculate x * y
-let xy = [];
-for (var i = 0; i < x.length; i++)
-{
-    xy.push(x[i]*y[i])
-}
+    let xy = [];
+    for (var i = 0; i < x.length; i++) {
+        xy.push(x[i] * y[i])
+    }
 
 //calculate sigma xy
-let sumxy = 0;
-for (i in xy){
-    sumxy = sumxy + parseFloat(xy[i]);
-}
+    let sumxy = 0;
+    for (i in xy) {
+        sumxy = sumxy + parseFloat(xy[i]);
+    }
 //calculate sigma x^2
-let sumx2 = 0;
-for (i in x2){
-    sumx2 = sumx2 + parseFloat(x2[i]);
-}
+    let sumx2 = 0;
+    for (i in x2) {
+        sumx2 = sumx2 + parseFloat(x2[i]);
+    }
 //calculate sigma y^2
-let sumy2 = 0;
-for (i in y2){
-    sumy2 = sumy2 + parseFloat(y2[i]);
-}
+    let sumy2 = 0;
+    for (i in y2) {
+        sumy2 = sumy2 + parseFloat(y2[i]);
+    }
+
 //correlation forumla
-function correlation(){
-    var r = (n*(sumxy) - (sumx)*(sumy))/Math.sqrt((n*sumx2-(sumx*sumx))*(n*sumy2-(sumy*sumy)));
-    return r
-}
+
+    var r = (n * (sumxy) - (sumx) * (sumy)) / Math.sqrt((n * sumx2 - (sumx * sumx)) * (n * sumy2 - (sumy * sumy)));
+
+    result = 'Correlation: ' + r + " <br><br>";
+    console.log(result);
+
 //regression formula.
-function regression(){
+
     //Calculating beta1 and beta2
-    var b1 = (sumxy - (n * avgx * avgy))/(sumx2-(n * avgx * avgx));
+    var b1 = (sumxy - (n * avgx * avgy)) / (sumx2 - (n * avgx * avgx));
     var b2 = avgy - (b1 * avgx);
     //calculating y linear regression
     y = 'y = ' + b1 + 'X + ' + b2;
-    return y;
+
+    result += 'Regression Equation: ' + y;
+    console.log(result);
+    return result;
 }
-console.log(correlation());
-console.log(regression());
+module.exports = calculate;
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const app = express();
+var storage = multer.memoryStorage();
+var upload = multer({ dest: 'uploads/' });
+var fs = require('fs');
+var calculate = require('./calculate');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+//Upload Data Sheet Route
+app.post('/stats', upload.array('datasheet', 2), function (req, res, next) {
+
+    //Use Calculate() to get correlation and regresssion values
+    let data1 = fs.readFileSync(req.files[0].path.toString(), 'utf8').split('\r\n');
+    let data2 = fs.readFileSync(req.files[1].path.toString(), 'utf8').split('\r\n');
+
+    //get the return values from function call.
+    let calculationResult = calculate(data1, data2);
+
+    //render the stats page with the values
+    res.render('stats', {result: calculationResult});
+});
+
+//Send user Json Data from RESTful Api
+app.post('/', function (req, res) {
+    res.redirect('/' + req.body.id);
+});
+
+//api page for instructions
+app.get('/api', function (req, res) {
+    res.render('api');
+});
+
+//stats when no data is being sent
+app.get('/stats', function (req, res) {
+    let calculationResult = '';
+    res.render('stats', {result: calculationResult});
+});
+
+//Restful API GET Route
+app.get('/:appId', (req, res, next) => {
+    let id = req.params.appId ;
+    res.status(200).json({
+        date: Date(),
+        ID: id,
+        messsage: 'This is my RESTful API server.'
+
+    });
+});
+app.post('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+//Starts the server on PORT 3000
+app.listen(3000, function () {
+    console.log('Lachlan\'s assignment 2 BCPR280 starting.....\nListening on Port 3000');
+});
